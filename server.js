5b78e60ab8548437565a40da4234aec788eec169
Oskar Krzyzak <oskar.krzyzak.ok@gmail.com>
2025-11-15 19:23:47 -0600

    Change server port from 3000 to 8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
 });
 
 // Cloud Run PORT fix:
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
